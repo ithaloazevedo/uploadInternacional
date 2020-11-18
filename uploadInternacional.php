ef050d206e4d8bce2f4a627ea995cb5c7d58a2c9
ithaloazevedo <ithaloazevedoo@gmail.com>
2020-11-18 16:27:31 -0300

    Get signatures

--- uploadInternacional.php
+++ uploadInternacional.php
@@ -13,5 +13,16 @@ $jsonData = file_get_contents("./report.json");
 if ($jsonData === false) {
     console_log("Não foi possível ler o arquivo!");
 }
+#Pegando as assinaturas
+$signatures = $decodedData->report->signatures->signature;
+if($signatures[1]){
+  for($j = 0; $j < sizeof($signatures);$j++){
+    console_log($signatures[$j]);
+  }
+  foreach ($signatures as $signature) {
+    console_log($signature);
+  }
+}
+
 
 ?>
